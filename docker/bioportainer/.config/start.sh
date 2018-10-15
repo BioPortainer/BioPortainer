701504d312a7d6518139f3ba9f356ed8ed12ac4c
Menegidio, F.B <fabianomenegidio@users.noreply.github.com>
2018-10-15 02:59:40 -0300

    Update start.sh

--- docker/bioportainer/.config/start.sh
+++ docker/bioportainer/.config/start.sh
@@ -4,5 +4,6 @@ set -e
 echo "Start BioPortainer Script Server"
 nohup python3 $HOME/script-server/launcher.py &
 sleep 20
+rm -rf $HOME/workdir/nohup.out
 echo "Start BioPortainer"
 $HOME/bioportainer
