1718bddd843ca655732d05fb75c6c8d9c188e886
Menegidio, F.B <fabianomenegidio@users.noreply.github.com>
2018-10-15 07:51:57 -0300

    Update ncbi-genome-download.sh

--- docker/bioportainer/script-server/conf/scripts/ncbi-genome-download.sh
+++ docker/bioportainer/script-server/conf/scripts/ncbi-genome-download.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
-ncbi-genome-download $@
+docker pull bioportainer/tools:ncbi-genome-download
+docker run -t --rm bioportainer/tools:ncbi-genome-download $@
+docker rmi bioportainer/tools:ncbi-genome-download
