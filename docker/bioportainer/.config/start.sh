fb507cb3135e555b32f9656f6d8006a7772c8302
Fabiano Menegidio <fabiano.menegidio@biology.bio.br>
2018-10-31 20:50:14 -0300

    bioportainer

--- docker/bioportainer/.config/start.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-set -e
-echo "Start BioPortainer Script Server"
-nohup python3 $HOME/script-server/launcher.py &
-sleep 20
-rm -rf $HOME/workdir/nohup.out
-echo "Start BioPortainer"
-$HOME/bioportainer
