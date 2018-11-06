939fc5b43ebcd5e31476e6b7437c879cae1ecfac
Fabiano Menegidio <fabiano.menegidio@biology.bio.br>
2018-11-05 23:59:34 -0200

    BioPortainer

--- Docker/Base/tools/script-server/conf/scripts/mkdir.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-mkdir -p $@
-echo "Local Repository Created"
--- Docker/Base/tools/script-server/conf/scripts/ncbi-genome-download.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-docker pull bioportainer/tools:ncbi-genome-download
-docker run -t --rm bioportainer/tools:ncbi-genome-download $@
-docker rmi bioportainer/tools:ncbi-genome-download
