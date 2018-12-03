4fe5ef4b5bf9b2c42dee4388b9b2434b443cc5e4
Menegidio, F.B <fabianomenegidio@users.noreply.github.com>
2018-12-03 12:01:41 -0200

    Update workdir_activate.sh

--- Modules/bioportainer-pipeline-runner/.tools/GUI/conf/scripts/workdir_activate.sh
+++ Modules/bioportainer-pipeline-runner/.tools/GUI/conf/scripts/workdir_activate.sh
@@ -4,4 +4,5 @@ cd $@
 work=$(pwd)
 echo $work > /.work
 echo "The directory $work was chosen as your workdir"
-chown -R 1000:1000 $HOME/workdir/data
+chown -R $UID:$UID /root/workdir/data 
+chown -R $UID:$UID /root/.nextflow/assets
