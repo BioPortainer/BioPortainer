a96d0bb1db77ab00de859415e8a4ac96db5533f2
Menegidio, F.B <fabianomenegidio@users.noreply.github.com>
2018-12-03 07:08:57 -0200

    Update git-clone.sh

--- Modules/bioportainer-pipeline-runner/.tools/GUI/conf/scripts/git-clone.sh
+++ Modules/bioportainer-pipeline-runner/.tools/GUI/conf/scripts/git-clone.sh
@@ -7,4 +7,5 @@ git $@
 echo " "
 echo " "
 echo "Cloning Repository Finish."
-chown -R 1000:1000 $HOME/workdir/data 
+chown -R 1000:1000 /root/workdir/data 
+chown -R 1000:1000 /root/.nextflow/assets
